e6a3a4a745f9aa6a3adff49aabe735b58c912dfc
wotrex <47450238+wotrex@users.noreply.github.com>
2021-05-29 15:40:13 +0300

    fix time

--- src/main/java/Code/assetstore/controllers/PaymentController.java
+++ src/main/java/Code/assetstore/controllers/PaymentController.java
@@ -24,6 +24,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import java.util.*;
+
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/pay")
